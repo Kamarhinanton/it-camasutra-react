df1c073f49804a67d521f7648482d44290883e8b
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-05-05 17:35:06 +0300

    feat: start project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Playstation_logo_colour.svg/2560px-Playstation_logo_colour.svg.png" alt="logo"/>
       </header>
+      <nav className='nav'>
+          <ul>
+              <li>
+                  <a href="#">
+                    messages
+                </a>
+              </li>
+              <li>
+                  <a href="#">
+                      profile
+                  </a>
+              </li>
+              <li>
+                  <a href="#">
+                    music
+                  </a>
+              </li>
+              <li>
+                  <a href="#">
+                      settings
+                  </a>
+              </li>
+          </ul>
+      </nav>
+      <div className='content'>
+        Main content
+          <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt=""/>
+      </div>
     </div>
   );
 }
