44dcdbb95eb41e48282e454491f2e4bdfb1ee321
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-06-21 11:48:49 +0300

    lesson 52

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,17 +7,17 @@ const Navbar = () => {
         <nav className={classes.nav}>
             <ul>
                 <li className={classes.item}>
-                    <NavLink  to="dialogs/" className={({ isActive }) => (isActive ? classes.active : classes.inactive)}>
+                    <NavLink  to="dialogs/" className={({ isActive }) => (isActive ? classes.active : undefined)}>
                         dialogs
                     </NavLink>
                 </li>
                 <li className={classes.item}>
-                    <NavLink to="profile/" className={({ isActive }) => (isActive ? classes.active : classes.inactive)}>
+                    <NavLink to="profile/" className={({ isActive }) => (isActive ? classes.active : undefined)}>
                         profile
                     </NavLink>
                 </li>
                 <li className={classes.item}>
-                    <NavLink to="users/" className={({ isActive }) => (isActive ? classes.active : classes.inactive)}>
+                    <NavLink to="users/" className={({ isActive }) => (isActive ? classes.active : undefined)}>
                         users
                     </NavLink>
                 </li>
--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import classes from './users.module.css'
 import axios from "axios";
 import userPhoto from '../../assets/images/user.png'
+//стврорення обʼєктів за допомогою літерала обʼєкту let a = {name: 'Dima', age: 31}, та класів let a new Man('Dima', 31)
 
 let Users = (props) => {
-    if(props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items)
-        });
+
+    let getUsers = () => {
+        if(props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                props.setUsers(response.data.items)
+            });
+        }
     }
     return (
         <div>
+            <button onClick={getUsers}>get users</button>
             {
                 props.users.map(u => <div key = {u.id}>
                     <span>
